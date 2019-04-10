5f42039815a59e99a7e4264523afe7617cec1e5d
Kirill Shcherbina <rabbichaim770@gmail.com>
2019-04-10 03:42:08 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -15,8 +15,8 @@ self.end_with?("!")
   end
 
   def count_sentences
-   self.split(".","?","!")
-binding.pry
+   array = self.split(".","?","!")
+   array.length
   #delete_if{|e| e.length == 0}
    #.reject! { |c| c.empty? }.count
   end
